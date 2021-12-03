506cf2ffded6dac0a40301d6fb0f121fd7f32df6
Juan Lago <lagojuanfrancisco@gmail.com>
2021-12-02 23:50:35 -0300

    Se agregaron las vistas Detail y ListNews

--- CLASE6/NewsPaPerApp/urls.py
+++ CLASE6/NewsPaPerApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.Index, name='index')
+    path('', views.Index, name='index'),
+    path('list/', views.ListNews, name='listNews'),
+    path('detail/', views.DetailNews, name='detailNew'),
 ]
--- CLASE6/NewsPaPerApp/views.py
+++ CLASE6/NewsPaPerApp/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def Index(request):
+    """
+    Al entrar al index vamos a ver las utimas noticias
+    """
     return render(
         request,
         'NewsPaperApp/index.html',
+        {}
+    )
+
+
+def ListNews(request):
+    """
+    """
+    return render(
+        request,
+        'NewsPaperApp/list-news.html',
+        {}
+    )
+
+
+def DetailNews(request):
+    """
+    """
+    return render(
+        request,
+        'NewsPaperApp/detail-news.html',
         {}
     )
